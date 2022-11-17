7049a5af6b8b780726b8eb65d946f11bc8619938
Zico007 <zicogunfolaju@gmail.com>
2022-11-17 01:21:21 +0100

    Added Headers

--- headers/string.h
+++ headers/string.h
@@ -5,5 +5,6 @@ int _strlen(const char *s);
 int _strcmp(const char *s1, const char *s2);
 char *_strcat(char *dest, const char *src);
 char *_strdup(const char *str);
+int count_str(char **stri);
 
 #endif
